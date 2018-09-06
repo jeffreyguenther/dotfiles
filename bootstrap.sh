6575b9722ddbd9c3727b86ebf4ed2ff8371eb00c
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2018-09-06 15:10:37 -0700

    Adds intallation for powerline and trash

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,13 @@
 # Install brew
 # Clone dotfiles
+# Install dotfiles
 # Install Brewfile
+brew bundle
+
+# Install trash
+# https://github.com/sindresorhus/trash-cli
+yarn global add trash-cli --prefix=/usr/local
+
+# Install powerline
+# https://github.com/powerline/powerline
+pip install powerline-status
